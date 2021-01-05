3588ed6c01616c56363d3fb88069d847355d67b3
Rekite <edward10379@gmail.com>
2021-01-05 23:25:24 +0900

    Initial commit

---
+++ FakePinterest/fakepinterest/accountapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from accountapp.views import hello_world
+
+app_name = "accountapp" #간편해짐
+
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello world'),
+]
