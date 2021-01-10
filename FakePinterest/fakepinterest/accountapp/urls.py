910a225e3be87a7e829ab383e99b1d11bfb6ab94
Rekite <edward10379@gmail.com>
2021-01-11 00:49:02 +0900

    modified post

--- FakePinterest/fakepinterest/accountapp/urls.py
+++ FakePinterest/fakepinterest/accountapp/urls.py
@@ -6,5 +6,5 @@ app_name = "accountapp" #간편해짐
 
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello world'),
+    path('hello_world/', hello_world, name='hello_world'),
 ]
--- FakePinterest/fakepinterest/accountapp/views.py
+++ FakePinterest/fakepinterest/accountapp/views.py
@@ -1,10 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+from django.urls import reverse
+
+
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'accountapp/hello_world.html', context={'text':'POST_METHOD'})
+
+        temp = request.POST.get('hello_world_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world')) #reverse: 해당하는 경로를 다시 만들어줌
+
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text':'GET_METHOD'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
