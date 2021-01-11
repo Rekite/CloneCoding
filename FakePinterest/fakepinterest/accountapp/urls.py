29c0ed82a4617896855d090ad326a8a1cab8928f
Rekite <edward10379@gmail.com>
2021-01-12 00:33:19 +0900

    createview

--- FakePinterest/fakepinterest/accountapp/urls.py
+++ FakePinterest/fakepinterest/accountapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp" #간편해짐
 
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    # 클래스형뷰는 as_view가 필요
+    path('create/', AccountCreateView.as_view(), name='create'),
+
 ]
--- FakePinterest/fakepinterest/accountapp/views.py
+++ FakePinterest/fakepinterest/accountapp/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
 from accountapp.models import HelloWorld
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 
 def hello_world(request):
@@ -21,3 +24,11 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm #html의 {{ form }}으로 바로 만들어줌
+    # 경로에 성공하면 어느 경로로 갈지 다시 지정해야 함
+    # class에서는 reverse를 사용할 수 없음/class형 뷰에서 reverse_lazy 사용
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
