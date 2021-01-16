1ed245901c3979e361779e29841174c761bc7b03
Rekite <edward10379@gmail.com>
2021-01-17 00:39:14 +0900

    Modified user

--- FakePinterest/fakepinterest/fakepinterest/urls.py
+++ FakePinterest/fakepinterest/fakepinterest/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # 이미지를 서버에서 페이지에 보여줄 수 있도록 하기 위한 설정
--- FakePinterest/fakepinterest/profileapp/views.py
+++ FakePinterest/fakepinterest/profileapp/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
 from profileapp.models import Profile
 from profileapp.forms import ProfileCreationForm
+from profileapp.decorators import profile_ownership_required
 
 
 class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:detail') -> pk를 받아야 해서 적용x
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -22,9 +24,18 @@ class ProfileCreateView(CreateView):
         temp_profile.save()
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+        #self.obejct -> Profile의 user의 pk를 넘겨줌
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+        # self.obejct -> Profile의 user의 pk를 넘겨줌
